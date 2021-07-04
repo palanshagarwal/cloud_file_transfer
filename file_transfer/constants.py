2fabb63a13f97c033dca374b367b336639411ebe
Palansh Agarwal <palanshagarwal@gmail.com>
2021-07-04 11:45:10 +0530

    filter files based on extension

--- file_transfer/constants.py
+++ file_transfer/constants.py
@@ -1,4 +1,4 @@
-DEFAULT_IMAGES = ['jpg', 'png', 'svg', 'webp']
+DEFAULT_IMAGES = ['jpg', 'png', 'svg', 'webp', 'jpeg']
 
 DEFAULT_MEDIA = ['mp3', 'mp4', 'mpeg4', 'wmv', '3gp', 'webm']
 
@@ -7,3 +7,6 @@ DEFAULT_DOCUMENT = ['doc', 'docx', 'csv', 'pdf']
 DEFAULT_UPLOAD_TO_AWS_S3 = DEFAULT_IMAGES + DEFAULT_MEDIA
 
 DEFAULT_UPLOAD_TO_GCS = DEFAULT_DOCUMENT
+
+AWS_S3 = 'aws_s3'
+GCS = 'gcs'
--- file_transfer/utils.py
+++ file_transfer/utils.py
@@ -17,18 +17,57 @@ def get_aws_s3_upload_list():
 def get_gcs_upload_list():
     return DEFAULT_UPLOAD_TO_GCS
 
-def get_file_path(dirpath, file_name):
-    return dirpath + file_name
+def get_file_ext(file_path):
+    filename, file_extension = os.path.splitext(file_path)
 
-def process_files(dirpath, file_list):
-    for file in file_list:
-        print('Uploading ' + get_file_path(dirpath, file))
+    # exclude '.' in extension name and return
+    return file_extension.lower()[1:]
+
+def get_file_path(dirpath, file_name):
+    return dirpath + '/' + file_name
+
+def upload_files(file_data, platform):
+    def upload_file(file, platform):
+        print('uploaded ' + file + ' to ' + platform)
+
+    if isinstance(file_data, list):
+        for file in file_data:
+            upload_file(file, platform)
+    else:
+        upload_file(file, platform)     
 
 def process_dir(path):
     # read all the files from the directory and its subdirectory
     # dirpath, dirnames, filenames = os.walk(path)
+    _upload_aws_s3_list = []
+    _upload_gcs_list = []
+
+    upload_aws_s3_ext_set = set(get_aws_s3_upload_list())
+    upload_gcs_ext_set = set(get_gcs_upload_list())
+
     for (dirpath, dirnames, filenames) in os.walk(path):
-        if filenames:
-            print('Uploading files in ' + dirpath)
-            process_files(dirpath, filenames)
-            print('*'*72)
+        for file in filenames:
+            file_path = get_file_path(dirpath, file)
+            file_ext = get_file_ext(file_path)
+            if file_ext in upload_aws_s3_ext_set:
+                _upload_aws_s3_list.append(file_path)
+            elif file_ext in upload_gcs_ext_set:
+                _upload_gcs_list.append(file_path)
+
+    if _upload_aws_s3_list:
+        upload_files(_upload_aws_s3_list, AWS_S3)
+
+    if _upload_gcs_list:
+        upload_files(_upload_gcs_list, GCS)
+
+
+
+
+
+
+
+
+
+
+
+
